5c7007c40dabc1e027773bd88222e2f2e0c8203b
Saswat <saswat.sam786@gmail.com>
2021-09-26 15:39:04 +0530

    empty commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,16 @@
 // import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import { render } from "react-dom";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       console.log("Latitude is :", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
     });
@@ -20,11 +19,10 @@ class Profile extends Component {
   render() {
     return (
       <div>
-        <h4>Using geolocation JavaScript API in React</h4>
         <Link to="">Home</Link>
       </div>
     );
   }
 }
 
-export default Profile
+export default Profile;
